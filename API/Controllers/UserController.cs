5cd42112465fa5c13fefcf159e71b7eeef5b1484
Hrishikesh Bhondve <hrishibhondve44@gmail.com>
2024-05-11 17:53:54 +0530

    InitialCommit-StructureBuild

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]   //api/user(UserController)
+public class UserController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UserController(DataContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        //all tables/db inside context
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+    [HttpGet("{id}")]   //api/user/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        //all tables/db inside context
+        return await _context.Users.FindAsync(id);
+    }
+}
